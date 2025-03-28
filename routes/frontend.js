dac61c7791973da1e4484e0b2f4a41437d42a728
farid ramy <faridramy2003@gmail.com>
2025-03-28 12:49:19 +0200

    add authentication pages html

--- routes/frontend.js
+++ routes/frontend.js
@@ -2,11 +2,23 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.render("index"));
-router.get("/cart", (req, res) => res.render("cart"));
-router.get("/category", (req, res) => res.render("category"));
-router.get("/checkout", (req, res) => res.render("checkout"));
-router.get("/product_details", (req, res) => res.render("product_details"));
-router.get("/account", (req, res) => res.render("account"));
+router.get("/", (req, res) => res.render("./index"));
+router.get("/signin", (req, res) => res.render("./authentication/signin"));
+router.get("/signup", (req, res) => res.render("./authentication/signup"));
+router.get("/cooperate_signin", (req, res) =>
+  res.render("./authentication/cooperate_signin")
+);
+router.get("/forget_password", (req, res) =>
+  res.render("./authentication/forget_password")
+);
+router.get("/two_step_verification", (req, res) =>
+  res.render("./authentication/two_step_verification")
+);
+
+router.get("/cart", (req, res) => res.render("./cart"));
+router.get("/shop", (req, res) => res.render("./shop"));
+router.get("/checkout", (req, res) => res.render("./checkout"));
+router.get("/product_details", (req, res) => res.render("./product_details"));
+router.get("/account", (req, res) => res.render("./account"));
 
 export default router;
